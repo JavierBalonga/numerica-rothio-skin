4044850d4455d6c0a3a304e651f973e3cc60042c
JavierBalonga <javierbalonga@gmail.com>
2023-08-31 08:48:26 -0300

    feat(*) explanation about how to include in OBS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { useMemo, useState } from "react";
+import { MouseEvent, useMemo, useState } from "react";
 import CopyIcon from "../components/icons/CopyIcon";
 
 export default function HomePage() {
   const [twitchUsername, setTwitchUsername] = useState("");
 
   const link = useMemo(() => {
-    return `${window.location.href}#/${twitchUsername || "rothiotome"}`;
+    return `${window.location.href}#/${twitchUsername}`;
   }, [twitchUsername]);
 
   const handleTwitchUsernameChange = (
@@ -14,14 +14,19 @@ export default function HomePage() {
     setTwitchUsername(e.target.value);
   };
 
-  const handleCopyLink = () => {
+  const handleCopyLink = (e: MouseEvent<HTMLButtonElement>) => {
     navigator.clipboard.writeText(link);
+    const element = e.currentTarget;
+    element.classList.add("before:animate-pulse");
+    setTimeout(() => {
+      element.classList.remove("before:animate-pulse");
+    }, 2000);
   };
 
   return (
     <div className="flex flex-col items-center gap-8 p-8 bg-slate-700/25 rounded-2xl">
-      <h1 className="text-4xl font-bold">
-        Numerica by <span className="underline">RothioTome</span>
+      <h1 className="text-4xl font-bold text-white">
+        Numerica <span className="underline text-primary">RothioTome</span> Skin
       </h1>
       <div className="relative rounded-md shadow-sm">
         <input
@@ -33,15 +38,28 @@ export default function HomePage() {
           onChange={handleTwitchUsernameChange}
         />
       </div>
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center gap-4">
+        <p className="text-lg text-center text-slate-400">
+          Para empezar a jugar, Copia la siguiente{" "}
+          <strong className="text-primary">URL</strong>
+          <br />y agregala como{" "}
+          <strong className="text-primary">Fuente de Navegador</strong> a tu OBS
+        </p>
         <button
-          className="flex flex-row gap-2 items-center text-xl font-bold"
+          className="relative flex flex-row gap-2 items-center text-xl font-bold before:content-['Copied!'] before:absolute before:top-1/2 before:-right-5 before:translate-x-full before:-translate-y-1/2 before:transition before:duration-300 before:ease-in-out before:rounded-md before:px-2 before:py-1 before:bg-slate-700 before:text-white before:ring-primary before:ring-inset before:opacity-0"
           onClick={handleCopyLink}
         >
           <CopyIcon className="text-2xl" />
           <span>{link}</span>
         </button>
-        <p className="text-lg">Copia este link on OBS para empezar a jugar.</p>
+        <p className="text-lg text-center text-slate-400">
+          Con <strong className="text-primary">400 x 400 (px)</strong> deberia
+          andar correctamente
+          <br />
+          luego puedes redimensionar si gustas
+          <br />Y por ultimo agregale un filtro de color de la siguiente manera:
+        </p>
+        <img src="/color-filter-example.png" />
       </div>
     </div>
   );
