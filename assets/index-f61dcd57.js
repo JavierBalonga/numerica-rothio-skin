8e2744b2a751481937d4aa2fe41e5aa4b22d38c0
JavierBalonga <JavierBalonga@users.noreply.github.com>
2023-12-09 00:37:34 +0000

    deploy: dd5e8caf5987ead3075976d56f60f0026b6aabdf

---
+++ assets/index-f61dcd57.js
@@ -0,0 +1,1 @@
+import{e as r,f as n,r as o,j as t}from"./index-42d8a62d.js";import{u}from"./index-fbb4b769.js";function c(){const{channel:e}=r(),s=n(),{setChannel:a}=u();return o.useEffect(()=>{e&&(a(e),s("/game"))},[e,a]),t.jsx(t.Fragment,{})}export{c as default};
