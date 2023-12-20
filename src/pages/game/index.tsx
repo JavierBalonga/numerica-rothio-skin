fd5c48453e907c40251988ace882fced7b6896fb
Laurick <javi.ethan@gmail.com>
2023-12-20 13:59:36 +0100

    changed regexp to keep track "<3" and something like that

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -95,7 +95,7 @@ export default function GamePage() {
       if (!userId || !newUser) return;
 
       const newNumber = Number(
-        message.replace(/[^\d\w]+$/, "").replace(/^[^\d\w]+/, "")
+        message.replace(/[^\D\d]+$/, "").replace(/^[^\D\d]+/, "")
       );
       const isFiniteNumber = isFinite(newNumber);
       const isIntegerNumber = newNumber % 1 === 0;
